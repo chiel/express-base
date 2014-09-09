de81d5302c6262d965ad3f1212b116ef65551354
Chiel Kunkels <chiel@kunkels.me>
2014-09-09 13:35:06 +0200

    add default middleware for handling 404/5xx errors

--- core/settings.js
+++ core/settings.js
@@ -10,7 +10,14 @@ app
 	.enable('strict routing')
 	.use(require('serve-static')(process.cwd() + '/public'))
 	.use(require('connect-slashes')())
-	.use(require('body-parser').urlencoded({extended: true}));
+	.use(require('body-parser').urlencoded({extended: true}))
+	.use(function(req, res){
+		res.status(404).render('errors/404');
+	})
+	.use(function(err, req, res, next){
+		console.error(err.stack);
+		res.status(500).render('errors/5xx');
+	});
 
 if (app.settings.env == 'development'){
 	swig.setDefaults({cache: false});
